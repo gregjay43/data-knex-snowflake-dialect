96ae7b0e5495c20475fb48a5d920c95a7f254213
gjay <greg.jay@viasat.com>
2022-09-27 16:15:54 -0700

    Remove file

--- src/knex.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "knex/lib/dialects/mysql/schema/mysql-tablecompiler";
-declare module "knex/lib/dialects/mysql/schema/mysql-compiler";
-declare module "knex/lib/dialects/mysql/query/mysql-querycompiler";
